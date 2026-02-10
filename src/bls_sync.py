7f2f0e8100780c95cebdef35819c1aa6fab48c2e
Milan Gabriel <mcwan33@gmail.com>
2026-02-10 10:46:31 +0530

    Initital project structure and scaffolding

---
+++ src/bls_sync.py
@@ -0,0 +1,7 @@
+"""
+Rearc Data Quest Part - 1 
+Local First Implementation of BLS Sync
+
+This script syncs BLS source files into a local directory that mirrors an S3 bucket structure.
+THe Logic is intenntionally backend-agnostic and can be mapped to 1:1 to S3 without code changes.
+"""
---
+++ src/population.py
@@ -0,0 +1,6 @@
+"""
+Rearc Data Quest Part - 2
+Local First Implementation of Population Data
+
+Fetches Population data from a public API and stores the responses as a JSON in a local directory that mirrors an S3 layout.
+"""
